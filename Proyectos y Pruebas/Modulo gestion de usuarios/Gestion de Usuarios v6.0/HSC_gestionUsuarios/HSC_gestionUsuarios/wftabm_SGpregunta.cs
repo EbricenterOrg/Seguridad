b406d4db9e892d5ac026cce469ebd33dcdef7206
criportillo <cristianportillobal@hotmail.com>
2014-11-03 23:09:20 -0600

    Gestion de usuarios v6.0
    Formulario pregunta arreglado

---
+++ Proyectos y Pruebas/Modulo gestion de usuarios/Gestion de Usuarios v6.0/HSC_gestionUsuarios/HSC_gestionUsuarios/wftabm_SGpregunta.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HSC_gestionUsuarios
+{
+    public partial class wftabm_SGpregunta : Form
+    {
+        private String Scodig = string.Empty;
+
+        public String Scodig1
+        {
+            get { return Scodig; }
+            set { Scodig = value; }
+        }
+
+        ArrayList alDatosEnviados = new ArrayList();
+        public wftabm_SGpregunta()
+        {
+            InitializeComponent();
+        }
+
+        private void wftabm_SGpregunta_Load(object sender, EventArgs e)
+        {
+            this.navegador1.btnNuevo.Enabled = false;
+            alDatosEnviados.Add(txtcod_preg);
+            alDatosEnviados.Add(txtnom_preg);
+            alDatosEnviados.Add(txtestado_preg);
+
+            navegador1.alDatosEntrada = alDatosEnviados;
+            navegador1.vIniciarNavegador();
+        }
+
+        private void cbonom_preg_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //txtnom_preg.Text = cbonom_preg.Text;
+        }
+
+        private void cboestado_preg_SelectedIndexChanged(object sender, EventArgs e)
+        {
+           
+           if (cboestado_preg.SelectedIndex.Equals(0))
+            {
+                txtestado_preg.Text = "1";
+            }
+            else
+            {
+                txtestado_preg.Text = "0";
+            }
+        }
+
+        private void txtestado_preg_TextChanged(object sender, EventArgs e)
+        {
+
+           if (txtestado_preg.Text=="1")
+            {
+                cboestado_preg.Text = "Activo";
+            }
+            else
+            {
+                cboestado_preg.Text = "Inactivo";
+            }
+        }
+
+        private void txtcod_preg_TextChanged(object sender, EventArgs e)
+        {
+            Scodig = txtcod_preg.Text; 
+        }
+
+        private void txtnom_preg_TextChanged(object sender, EventArgs e)
+        {
+            //cbonom_preg.Text = txtnom_preg.Text;
+        }
+
+        private void navegador1_btnNuevo_AfterClick(object sender, EventArgs e)
+        {
+          
+            this.txtcod_preg.Enabled = true;
+            this.txtnom_preg.Enabled = true;
+            this.cboestado_preg.Enabled = true;
+
+        }
+    }
+}
