245aacb8d55736e6cb838feb95cb6364921c05ec
Leonel L. Pazos <leolpazos071@hotmail.com>
2014-11-15 22:06:26 -0600

    Ultima Actualizacion

--- Proyectos y Pruebas/Modulo de Correos/EnvioCorreos/Correo.cs
+++ Proyectos y Pruebas/Modulo de Correos/EnvioCorreos/Correo.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿/* Desarrollador: Jaime Ivan Muñoz
+ * Fecha: 15/10/2014
+ */
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Mail;
--- Proyectos y Pruebas/Modulo de Seguridad 3.0/Modulo de Seguridad 3.0/Modulo de Seguridad 3.0/Negocio/N_InicioSesion.cs
+++ Proyectos y Pruebas/Modulo de Seguridad 3.0/Modulo de Seguridad 3.0/Modulo de Seguridad 3.0/Negocio/N_InicioSesion.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿/* Desarrollador: Jorge Leonel Lam
+ * Fecha: 01/10/2014
+ */
+
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
